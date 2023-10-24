886d764de06356101ff378f67f9a5adc579ab74e
minaje0917 <k050918@naver.com>
2023-10-24 20:14:53 +0900

    :tada: Begin :: Begin GOMS-Modular

---
+++ Targets/GOMSModularKit/Tests/GOMSModularKitTests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class GOMSModularKitTests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("GOMSModularKit", "GOMSModularKit")
+    }
+}
---
+++ Targets/GOMSModularUI/Tests/GOMSModularUITests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class GOMSModularUITests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("GOMSModularUI", "GOMSModularUI")
+    }
+}
