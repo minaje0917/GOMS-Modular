6f30181aa72b707c37df29f88f7744fa043bd21e
minaje0917 <k050918@naver.com>
2023-10-26 16:47:52 +0900

    :seedling: Add :: BaseViewController

---
+++ Projects/Feature/BaseFeature/Sources/BaseViewController.swift
@@ -0,0 +1,53 @@
+import UIKit
+import RxCocoa
+import RxSwift
+import Then
+import DesignSystem
+
+class BaseViewController<T>: UIViewController {
+    let reactor: T
+    var disposeBag = DisposeBag()
+    let bounds = UIScreen.main.bounds
+    
+    init(_ reactor: T) {
+        self.reactor = reactor
+        super .init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.tintColor = DesignSystemAsset.Color.black.color
+        view.backgroundColor = DesignSystemAsset.Color.bg.color
+        self.navigationItem.backButtonTitle = ""
+        addView()
+        setLayout()
+        bind(reactor: reactor)
+    }
+    
+    func addView() {
+        
+    }
+    
+    func setLayout() {
+        
+    }
+    
+    func bind(reactor: T) {
+        bindView(reactor: reactor)
+        bindAction(reactor: reactor)
+        bindState(reactor: reactor)
+    }
+    
+    func bindView(reactor: T) {}
+    func bindAction(reactor: T) {}
+    func bindState(reactor: T) {}
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+
+}
